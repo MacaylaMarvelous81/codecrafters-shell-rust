21088ac88a6938facc78bd5603376b0f87c81be7
Jomar Milan <jomarm@jomarm.com>
2024-09-29 20:10:49 -0700

    Revert "Associate command logic with commands by HashMap"
    This reverts commit 06e6c675b52bb8e7bdebe910c0b831482533c073.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-use std::collections::HashMap;
-
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    let mut commands: HashMap<&str, fn(&mut dyn Iterator<Item=&str>) -> Option<u8>> = HashMap::new();
-    commands.insert("exit", |args| {
-        Some(args.next().map(|arg| {
-            arg.parse().unwrap_or(0)
-        }).unwrap_or(0))
-    });
-    commands.insert("echo", |args| {
-        println!("{}", args.collect::<Vec<&str>>().join(" "));
-        None
-    });
-    commands.insert("type", |args| {
-        let subject = args.next();
-
-        if subject == Some("exit") || subject == Some("echo") || subject == Some("type") {
-            println!("{} is a shell builtin", subject.unwrap_or(""))
-        } else {
-            println!("{}: not found", subject.unwrap_or(""))
-        }
-        None
-    });
-
     // Wait for user input
     let stdin = io::stdin();
     let mut status: Option<u8> = None;
@@ -41,15 +18,29 @@ fn main() {
 
         match command {
             Some(command) => {
-                status = match commands.get(command) {
-                    Some(func) => func(&mut args),
-                    None => {
-                        if command != "" {
-                            println!("{}: command not found", command);
-                        }
-                        None
+                match command {
+                    "exit" => {
+                        status = Some(args.next().map(|arg| {
+                            arg.parse().unwrap_or(0)
+                        }).unwrap_or(0));
                     }
-                };
+                    "echo" => {
+                        println!("{}", args.collect::<Vec<&str>>().join(" "));
+                    }
+                    "type" => {
+                        let subject = args.next();
+
+                        if subject == Some("exit") || subject == Some("echo") || subject == Some("type") {
+                            println!("{} is a shell builtin", subject.unwrap_or(""))
+                        } else {
+                            println!("{}: not found", subject.unwrap_or(""))
+                        }
+                    }
+                    "" => {}
+                    _ => {
+                        println!("{}: command not found", command);
+                    }
+                }
             }
             None => {}
         }
